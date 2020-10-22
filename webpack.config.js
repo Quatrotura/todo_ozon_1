7d72952f8cdbe1b2fb9857f0806c7d427be1dfd5
Sergey Yakubov <SYakubov@MacBook-Pro-Sergey.local>
2020-10-23 02:16:46 +0300

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+
 
 module.exports = {
     mode: 'development',
@@ -8,5 +10,14 @@ module.exports = {
      path: path.resolve(__dirname, 'dist'),
      filename: 'index.bundle.js'
   },
-watch: true
+watch: true,
+plugins: [
+    new BrowserSyncPlugin({
+      // browse to http://localhost:3000/ during development,
+      // ./public directory is being served
+      host: 'localhost',
+      port: 30,
+      server: { baseDir: ['...2nd attempt/js'] }
+    })
+  ]
 };
